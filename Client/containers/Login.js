71a4717c808dd980f98eb9212805b7b2e6a1b497
Joe Fazzino <joe@fluid.work>
2017-12-10 18:01:14 +0000

    Login changes with pickers

--- Client/containers/Login.js
+++ Client/containers/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, Image, TouchableHighlight, TextInput, ActivityIndicator } from 'react-native';
+import { View, TouchableOpacity, Text, Image, TouchableHighlight, TextInput, ActivityIndicator, Picker } from 'react-native';
 import { connect } from 'react-redux';
 import Swiper from 'react-native-swiper';
 import { Button, Avatar } from 'react-native-elements';
@@ -28,7 +28,13 @@ export class Login extends React.Component {
             isLookingForHouse: false,
             isLoggingIn: false,
             isLoggedIn: false,
-            profile: {}
+            profile: {},
+            minPrice: 300,
+            maxPrice: 450,
+            minEnabled: false,
+            maxEnabled: false,
+            genderEnabled: false,
+            genderPreference: "No Preference"
         }
     }
 
@@ -88,7 +94,10 @@ export class Login extends React.Component {
                         <Image style={{ width: 250, height: 250 }} source={Box} /> 
                     </View>
                     <View style={ login.pageFooter }>
-                        <Button title={'Next'} onPress={() => this.homeSwiper.scrollBy(1, true)} buttonStyle={ base.buttonStyle } />
+                        <Button title={'New Account'} onPress={() => this.homeSwiper.scrollBy(1, true)} buttonStyle={ base.buttonStyle } />
+                        <TouchableHighlight onPress={this.loginToFacebook}>
+                            <Text style={{ color: '#1ebde7', textDecorationLine: 'underline', marginTop: 10 }}>Already Got an Account? Login</Text>
+                        </TouchableHighlight>
                     </View>
                 </View>
 
@@ -145,6 +154,30 @@ export class Login extends React.Component {
                             buttonStyle={[ base.buttonStyle, this.state.isLoggedIn ? { backgroundColor: Colors.brandSuccessColor } : { backgroundColor: Colors.facebookBlue }]} />
                     </View>         
                 </View>       
+                <View style={ login.page }>
+                    <View style={ login.headingWrapper }>
+                        <Text style={ login.headingText }>Enter information for your profile</Text>
+                    </View>
+                    <View style={[ login.mainContent, { flex: 4, justifyContent: 'flex-start', alignItems: 'center' } ]}>
+                        <Avatar 
+                            xlarge={true} 
+                            rounded={true} 
+                            source={this.state.isLoggedIn ? {uri: this.state.profile.get('imageUrl')} : facebook_template } />
+                        <Text style={{ fontSize: 24, marginTop: 5 }}>{this.state.isLoggedIn ? this.state.profile.get('name') : 'John Smith'}</Text>
+                        <Text style={[ login.labelText, { marginTop: 20 } ]}>Bio</Text>
+                        <TextInput placeholder={'Enter a short description of yourself'}
+                            multiline={true}
+                            numberOfLines={2}
+                            style={{ color: Colors.textHighlightColor, width: 300, fontSize: 18, borderBottomWidth: 1, borderColor: Colors.grey, }} />
+                            {/* social links would be good here */}
+                    </View>
+                    <View style={ login.pageFooter }>
+                        <Button
+                            title={'Confirm'}
+                            onPress={() => this.homeSwiper.scrollBy(1, true)}
+                            buttonStyle={[ base.buttonStyle ]} />
+                    </View>
+                </View>
 
                 {this.renderHouseOrProfileSetup()}
 
@@ -160,30 +193,76 @@ export class Login extends React.Component {
     }
 
     renderHouseOrProfileSetup() {
-        console.log(this.state.profile);
         if (this.state.isLookingForHouse) {
-            return [
-                <View style={ login.page } key={'UserProfile'}>
+            return (
+                <View style={ login.page } key={'Preferences'}>
                     <View style={ login.headingWrapper }>
-                        <Text style={ login.headingText }>Enter information for your profile</Text>
+                        <Text style={ login.headingText }>Enter your preferences for a house</Text>
                     </View>
-                    <View style={[ login.mainContent, { justifyContent: 'flex-start' } ]}>
-                        <Avatar 
-                            xlarge={true} 
-                            rounded={true} 
-                            source={this.state.isLoggedIn ? {uri: this.state.profile.get('imageUrl')} : facebook_template } />
-                        <Text>{this.state.isLoggedIn ? this.state.profile.get('name') : 'John Smith'}</Text>
-                        <Text style={ login.labelText }>Bio</Text>
-                        <TextInput placeholder={'Enter a short description of yourself here'}
-                            style={{ color: Colors.textHighlightColor, width: 200, fontSize: 18, borderBottomWidth: 1, borderColor: Colors.grey }} />
+                    <View style={[ login.mainContent, {justifyContent: 'flex-start'} ]}>
+                        <View style={{ marginBottom: 20 }}>
+                            <Text style={ login.labelText }>Minimum Price (incl. bills)</Text>
+                            <TouchableOpacity style={{ width: 200, borderBottomWidth: 1, borderColor: Colors.grey  }} onPress={() => this.setState({ minEnabled: true })}>
+                                <Text style={{ color: Colors.textHighlightColor, fontSize: 18 }}>£{this.state.minPrice}</Text>
+                            </TouchableOpacity>
+                            {this.state.minEnabled ?
+                            <Picker
+                                enabled={this.state.minEnabled}
+                                selectedValue={this.state.minPrice}
+                                onValueChange={(itemValue, itemIndex) => this.setState({ minPrice: itemValue },() => setTimeout(() => this.setState({minEnabled: false}), 100))}
+                                prompt={'Min Price'}>
+                                <Picker.Item label={"£200"} value={200} />
+                                <Picker.Item label={"£250"} value={250} />
+                                <Picker.Item label={"£300"} value={300} />
+                                <Picker.Item label={"£350"} value={350} />
+                                <Picker.Item label={"£400"} value={400} />
+                            </Picker> : <View/> }
+                        </View>
+                        
+                        <View style={{ marginVertical: 20 }}>
+                            <Text style={ login.labelText }>Maximum Price (incl. bills)</Text>
+                            <TouchableOpacity style={{ width: 200,borderBottomWidth: 1, borderColor: Colors.grey  }} onPress={() => this.setState({ maxEnabled: true })}>
+                                <Text style={{ color: Colors.textHighlightColor, width: 200, fontSize: 18 }}>£{this.state.maxPrice}</Text>
+                            </TouchableOpacity>
+
+                            {this.state.maxEnabled ?
+                            <Picker
+                                enabled={this.state.maxEnabled}
+                                selectedValue={this.state.maxPrice}
+                                onValueChange={(itemValue, itemIndex) => this.setState({ maxPrice: itemValue },() => setTimeout(() => this.setState({maxEnabled: false}), 100))}
+                                prompt={'Max Price'}>
+                                <Picker.Item label={"£350"} value={350} />
+                                <Picker.Item label={"£400"} value={400} />
+                                <Picker.Item label={"£450"} value={450} />
+                                <Picker.Item label={"£500"} value={500} />
+                                <Picker.Item label={"£550"} value={550} />
+                            </Picker> : <View/> }
+                        </View>
+
+                        <View style={{ marginTop: 20 }}>
+                            <Text style={ login.labelText }>Gender Majority</Text>
+                            <TouchableOpacity style={{ width: 200,borderBottomWidth: 1, borderColor: Colors.grey  }} onPress={() => this.setState({ genderEnabled: true })}>
+                                <Text style={{ color: Colors.textHighlightColor, width: 200, fontSize: 18 }}>{this.state.genderPreference}</Text>
+                            </TouchableOpacity>
+
+                            {this.state.genderEnabled ?
+                                <Picker
+                                    enabled={this.state.genderEnabled}
+                                    selectedValue={this.state.genderPreference}
+                                    onValueChange={(itemValue, itemIndex) => this.setState({ genderPreference: itemValue },() => setTimeout(() => this.setState({genderEnabled: false}), 100))}
+                                    prompt={'Max Price'}>
+                                    <Picker.Item label={"Male"} value={"Male"} />
+                                    <Picker.Item label={"Female"} value={"Female"} />
+                                    <Picker.Item label={"No Preference"} value={"No Preference"} />
+                                </Picker> : <View/> }
+                        </View>
+                        {/* location would be good with with defaulting to current location */}
                     </View>
                     <View style={ login.pageFooter }>
-                        <Button
-                            title={'Confirm'}
-                            buttonStyle={[ base.buttonStyle ]} />
+                        <Button title={'Confirm'} buttonStyle={ base.buttonStyle } />
                     </View>
                 </View>
-            ];
+            );
         } else {
             return [
                 <View style={ login.page } key={'HouseID'}>
