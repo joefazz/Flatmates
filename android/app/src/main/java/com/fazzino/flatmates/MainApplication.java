a5d720c6cc1a0569baafd80759e72b0313e5380c
Joe Fazzino <joe@fluid.work>
2018-03-30 12:54:47 +0100

    Remove facebook sdk and rnshinebutton

--- android/app/src/main/java/com/fazzino/flatmates/MainApplication.java
+++ android/app/src/main/java/com/fazzino/flatmates/MainApplication.java
@@ -5,30 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.facebook.react.ReactApplication;
-import ui.shine.RNShineButtonPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.mapbox.rctmgl.RCTMGLPackage;
 import com.wix.interactable.Interactable;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookSdk;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
-    private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
-
-    protected static CallbackManager getCallbackManager() {
-      return mCallbackManager;
-    }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -38,17 +28,14 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      mCallbackManager = new CallbackManager.Factory().create();
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
-            new RNShineButtonPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
             new PickerPackage(),
             new RCTMGLPackage(),
-            new Interactable(),
-            new FBSDKPackage(mCallbackManager)
-      );
+            new Interactable()
+        );
     }
 
     @Override
@@ -65,7 +52,6 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    FacebookSdk.sdkInitialize(getApplicationContext());
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
