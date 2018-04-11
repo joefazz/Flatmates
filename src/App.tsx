1b3558ef1ba05c761f1e59634dab7201bc402bdf
Joe Fazzino <joe@fluid.work>
2018-04-11 16:34:28 +0100

    Add push config

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,15 @@ import Mapbox from '@mapbox/react-native-mapbox-gl';
 import MapboxClient from 'mapbox/lib/services/geocoding';
 import * as React from 'react';
 import { ApolloProvider } from 'react-apollo';
-import { AsyncStorage, BackHandler, StatusBar } from 'react-native';
+import { AsyncStorage, BackHandler } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
-
-import Splash from '../Assets/splash_screen.png';
 import client from './Client';
 import { MAPBOX_API_TOKEN } from './consts/strings';
 import RootNavigation from './navigators/Root';
 import store from './redux/store';
-import { base } from './styles';
+import OneSignal from 'react-native-onesignal';
 
 Mapbox.setAccessToken(MAPBOX_API_TOKEN);
 
@@ -47,13 +45,33 @@ export default class Root extends React.Component<Props, State> {
         BackHandler.addEventListener('hardwareBackPress', () =>
             store.dispatch(NavigationActions.back())
         );
+
+        OneSignal.addEventListener('received', this.onReceivePush);
+        OneSignal.addEventListener('opened', this.onOpenPush);
+        OneSignal.addEventListener('ids', this.saveIds);
     }
 
     componentWillUnmount() {
         BackHandler.removeEventListener('hardwareBackPress', () =>
             store.dispatch(NavigationActions.back())
         );
+
+        OneSignal.removeEventListener('received', this.onReceivePush);
+        OneSignal.removeEventListener('opened', this.onOpenPush);
+        OneSignal.removeEventListener('ids', this.saveIds);
     }
+
+    onReceivePush = (notification) => {
+        console.log(notification);
+    };
+
+    onOpenPush = (openResult) => {
+        console.log(openResult);
+    };
+
+    saveIds = (device) => {
+        console.log('Device info: ', device);
+    };
 
     render() {
         return (
