bf50aa3e38bcc5616dd68143417e8601e724678f
Joe Fazzino <joe@fluid.work>
2018-05-16 15:18:31 +0100

    Fix bad error

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -5,10 +5,12 @@ import feed from './feedReducer';
 import profile from './profileReducer';
 import login from './loginReducer';
 import applications from './applicationReducer';
+import { apolloReducer } from 'apollo-cache-redux';
 import chat from './chatReducer';
 
 // File References
 export default combineReducers({
+    apollo: apolloReducer,
     login,
     profile,
     feed,
