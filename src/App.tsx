3b2e5450e416fcd8480034c81f6673edc7997ae3
JoeFazz <joseph@fazzino.net>
2018-09-14 18:39:49 +0100

    Attempt to fix pointer crash

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ Mapbox.setAccessToken(MAPBOX_API_TOKEN);
 Sentry.config(
     'https://ff264cadb5f4403d8b6dbfd86e610646:41e9999cbe0745ab9fc6d069d656fee0@sentry.io/1216809',
     {
-        deactivateStacktraceMerging: false
+        deactivateStacktraceMerging: true
     }
 ).install();
 
